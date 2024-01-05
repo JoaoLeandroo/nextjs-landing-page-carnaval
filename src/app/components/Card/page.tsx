2db26b2219d1c4d0da6cbedcd2f3c02d62f865eb
joaoleandro <new.joaoleandro@gmail.com>
2024-01-05 15:29:23 -0300

    removendo alguns comentarios

--- src/app/components/Card/page.tsx
+++ src/app/components/Card/page.tsx
@@ -1,17 +1,7 @@
-import Image from "next/image"
-
-// interface infosCardProps {
-//     href: string;
-//     alt: string;
-//     title: string;
-//     text: string;
-//     localization: string;
-// }
-
 interface CardsProps {
     children: React.ReactNode;
 }
-const Card: React.FC<CardsProps> = ({ children }) => {
+const Card: React.FC<CardsProps> = ({children}) => {
     return(
         <section className="w-[384px] h-[323px] rounded-xl bg-[#FFFFFF] border border-solid border-gray-500">
             {children}
